32a6a36d1a47d3e33037fcd6f5e6279336121c13
phuennae <113125472+phuennae@users.noreply.github.com>
2023-11-22 23:38:18 +0700

    Revert "finished (#3)"
    This reverts commit 7e2707f5f7358b9da78535de0a6a07aec37f773c.

--- src/Main.java
+++ src/Main.java
@@ -2,19 +2,16 @@
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        AirPurifier myAirPurifier = new AirPurifier("ModelZ", "650610791");
-        myAirPurifier.TurnOnTime(5);
-        myAirPurifier.TurnOffTime(6);
-        myAirPurifier.turnOn();
-        myAirPurifier.setSpeed(75);
-        myAirPurifier.setSpeed(101);
-        myAirPurifier.turnOn();
-        myAirPurifier.setSpeed(10);
-        System.out.println(myAirPurifier.model);
-        System.out.println(myAirPurifier.serialNo);
-        System.out.println(myAirPurifier.speed);
-        System.out.println(AirPurifier.mostmade());
-        System.out.println(AirPurifier.PopColour);
-        myAirPurifier.turnOff();
+        // Press Alt+Enter with your caret at the highlighted text to see how
+        // IntelliJ IDEA suggests fixing it.
+        System.out.print("Hello and welcome!");
+
+        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
+        for (int i = 1; i <= 5; i++) {
+
+            // Press Shift+F9 to start debugging your code. We have set one breakpoint
+            // for you, but you can always add more by pressing Ctrl+F8.
+            System.out.println("i = " + i);
+        }
     }
 }
